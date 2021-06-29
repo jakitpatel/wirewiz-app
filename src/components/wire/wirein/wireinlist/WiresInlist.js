535cd6d8374b63bd7a3c39781f50b6386631aa35
Jakit <jakit@Jakits-MacBook-Pro.local>
2021-06-29 19:12:46 +0530

    Added generate ofac file in wireout

--- src/components/wire/wirein/wireinlist/WiresInlist.js
+++ src/components/wire/wirein/wireinlist/WiresInlist.js
@@ -423,7 +423,7 @@ function WiresInlist(props) {
           }*/
           let wirePostID = batchRec.wirePostID;
           //let filterUrl = "((vAcc = '"+account+"') and (wirePostID = '"+wirePostID+"') and (excludeOFAC is NULL) and (excludeFISERV is null) and (errorMsg is NULL) and ((businessErrorMsg is NULL) or ((businessErrorMsg is not NULL) and (overrideFlag is not NULL))))";
-          let filterUrl = "(wirePostID = '"+wirePostID+"')";
+          let filterUrl = "((wirePostID = '"+wirePostID+"') and (excludeFED is NULL))";
           url += "&filter="+encodeURIComponent(filterUrl);
         } else if(batchRec.fromView && batchRec.fromView==="wireOutCompleted"){
           let wirePostID = batchRec.wirePostID;

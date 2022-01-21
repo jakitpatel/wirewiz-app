e28eb3acb70bde6a719fe67f0473b7beb8d52f11
Jakit <jakit@Jakits-MacBook-Pro.local>
2022-01-21 13:30:49 +0530

    Modified const file, did sort change on wirelist, wirebatch

--- src/reducer/wiresReducer.js
+++ src/reducer/wiresReducer.js
@@ -1,7 +1,7 @@
 const wiresReducer = (state = { 
     wires:[], totalCount:0,
     pageIndex:0, pageSize:10, 
-    sortBy:[], filters:[], 
+    sortBy:[{ id: "wireID", desc: true }], filters:[], 
     extFiltersArr :[], isListFiltered:false, 
     backToList:false 
 }, action) => {

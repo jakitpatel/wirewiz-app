eb7df3de4792f47f39ab69440e9a58c8f5804735
Jakit <jakit@Jakits-MacBook-Pro.local>
2022-12-26 18:15:52 +0530

    Added new column as excludeFromOFAC

--- src/components/wire/wirebatch/WireBatch.js
+++ src/components/wire/wirebatch/WireBatch.js
@@ -4,18 +4,24 @@ import { Link } from "react-router-dom";
 import WireBatchListview from "./WireBatchListview";
 import * as Icon from "react-feather";
 import "./WireBatch.css";
-//import axios from 'axios';
+import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import SelectColumnFilter from './../../Filter/SelectColumnFilter.js';
 //import {API_KEY, WireBatch_Url} from './../../../const';
-//const {API_KEY, WireBatch_Url} = window.constVar;
+const {API_KEY, env, ExecServiceLock_Url} = window.constVar;
 
 function WireBatch(props) {
   const [loading, setLoading] = useState(false);
   const [wirebatchlist, setWirebatchlist] = useState([]);
   const [isRefresh, setIsRefresh] = useState(false);
   const location = useLocation();
+
+  const { session_token } = useSelector(state => {
+      return {
+          ...state.userReducer
+      }
+  });
 
   let today = new Date();
   let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
@@ -153,6 +159,27 @@ function WireBatch(props) {
       field: "numError",
       Header: "NumError",
       accessor: "numError"
+    },
+    {
+      name: "excludeFromOFAC",
+      field: "excludeFromOFAC",
+      Header: "excludeFromOFAC",
+      accessor: "excludeFromOFAC",
+      Cell: obj => {
+        //console.log(obj.row);
+        let wireListObj = obj.row.original;
+        let excludeFromOFAC = wireListObj.excludeFromOFAC;
+        let shoExcludeFromOFAC = false;
+        if(excludeFromOFAC === true || excludeFromOFAC === "true"){
+          shoExcludeFromOFAC = true;
+        }
+        //console.log(colorCode);
+        return (
+          <button type="button" onClick={(e)=>{onExcludeFromOFAC(e, wireListObj)}} className={`btn btn-link btn-sm ${shoExcludeFromOFAC ? "" : "disabled"}`}>
+            <Icon.Send />
+          </button>
+        );
+      }
     }
   ];
   /*
@@ -178,6 +205,57 @@ function WireBatch(props) {
     return () => { ignore = true };
   }, [session_token,location.key]);
   */
+   const onExcludeFromOFAC = async (e, wireBatchObj) => {
+    console.log("Called Wire Batch excludeFromOFAC");
+    console.log(wireBatchObj);
+    /*console.log("sending : "+sending);
+    if(sending===true){
+      return false;
+    }*/
+    const options = {
+      headers: {
+        'X-DreamFactory-API-Key': API_KEY,
+        'X-DreamFactory-Session-Token': session_token
+      }
+    };
+    let dataArr = [{"wirePostID": wireBatchObj.wirePostID}];
+    
+    let data = {
+      "resource"  : dataArr,
+      "service"   : "wirePost7" 
+    };
+    //let url = WireInExport_Url;
+    let url = ExecServiceLock_Url;
+    console.log(url);
+    try {
+      //setSending(true);
+      setLoading(true);
+      let res = await axios.post(url, data, options);
+      console.log(res.data);
+      //setSending(false);
+      setLoading(false);
+      if(res.data.error){
+        alert(res.data.error.message);
+      } else {
+        setIsRefresh(!isRefresh);
+      }
+    } catch (error) {
+      console.log(error); // from creation or business logic
+      console.log(error.response);
+      if (401 === error.response.status) {
+        // handle error: inform user, go to login, etc
+        let res = error.response.data;
+        alert(res.error.message);
+      } else {
+        let res = error.response.data;
+        console.log("Error message: "+res.error.message);
+        alert(res.error.message);
+      }
+      setLoading(false);
+      //setSending(false);
+    }    
+  }
+
   console.log("Properties", props);
   console.log("wireBatchLoad : "+location.key);
   const initialState = {
